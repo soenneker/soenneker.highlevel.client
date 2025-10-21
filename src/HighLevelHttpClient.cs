22bda668b8a8ab5c4a49dda7f4ef4d6d6b14caa7
Jake Soenneker <jake@soenneker.com>
2025-10-21 09:57:31 -0500

    version

--- src/HighLevelHttpClient.cs
+++ src/HighLevelHttpClient.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Soenneker.Dtos.HttpClientOptions;
 using Soenneker.HighLevel.Client.Abstract;
 using Soenneker.Utils.HttpClientCache.Abstract;
@@ -11,12 +13,14 @@ namespace Soenneker.HighLevel.Client;
 public sealed class HighLevelHttpClient : IHighLevelHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
+    private readonly string _version;
 
     private const string _prodBaseUrl = "https://services.leadconnectorhq.com/";
 
-    public HighLevelHttpClient(IHttpClientCache httpClientCache)
+    public HighLevelHttpClient(IHttpClientCache httpClientCache, IConfiguration config)
     {
         _httpClientCache = httpClientCache;
+        _version = config.GetValue<string>("HighLevel:Version") ?? "2021-07-28";
     }
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
@@ -24,7 +28,11 @@ public sealed class HighLevelHttpClient : IHighLevelHttpClient
         return _httpClientCache.Get(nameof(HighLevelHttpClient), () => {
             var options = new HttpClientOptions
             {
-                BaseAddress = _prodBaseUrl
+                BaseAddress = _prodBaseUrl,
+                DefaultRequestHeaders = new Dictionary<string, string>
+                {
+                    {"Version", _version}
+                }
             };
             return options;
         }, cancellationToken: cancellationToken);
