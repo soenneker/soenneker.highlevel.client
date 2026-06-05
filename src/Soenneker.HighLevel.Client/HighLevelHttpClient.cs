52e061b0f57526049d66d64f5a87d6a5b7f63c0b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:17 -0500

    xml commenting

--- src/Soenneker.HighLevel.Client/HighLevelHttpClient.cs
+++ src/Soenneker.HighLevel.Client/HighLevelHttpClient.cs
@@ -37,11 +37,18 @@ public sealed class HighLevelHttpClient : IHighLevelHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(HighLevelHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(HighLevelHttpClient));
